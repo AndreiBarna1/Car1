3deada4e079244fe149655bd693976b95bf00d53
Артём_ПК <_@WIN-1CE93UBQJN5>
2020-06-08 20:15:01 +0400

    Добавьте файлы проекта.

---
+++ CarClass/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Passengercar
+    {
+        public double price { get; set; }
+        public int age { get; set; }
+        public string place { get; set; }
+        public string name { get; set; }
+
+        public void GetPassengercarInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Лет с момента покупки: {age}| |Страна производства: {place}|");
+        }
+    }
+
+
+    class Truck : Passengercar
+    {
+        public string type { get; set; }
+
+        public virtual void GetTruckInfo()
+        {
+            Console.WriteLine($"Имя: {name}| |Лет с момента покупки: {age}| |Место: {place}| |Цена автомобиля: {price}|");
+        }
+    }
+}
---
+++ CarClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Passengercar Passengercar = new Passengercar();
+            Passengercar.name = "Hyundai Solaris";
+            Passengercar.price = 600000;
+            Passengercar.age = 1;
+            Passengercar.place = "Корея";
+
+            Passengercar.GetPassengercarInfo();
+
+            Truck truck = new Truck();
+            truck.name = "МАЗ-5440";
+            truck.place = "Минск";
+            truck.price = 2500000;
+            truck.age =  5;
+
+
+            truck.GetTruckInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
